ac691356cad59efc9fdcd54c7bff9a8b6d7801c9
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-08-01 16:29:03 -0700

    add(App): data pulled success

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Home from "./pages/Home";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("http://localhost:8000/api");
+      setItems(data.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <div></div>
+      <Home items={items} />
     </div>
   );
 }
