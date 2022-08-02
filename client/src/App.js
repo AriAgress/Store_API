3b944a447bffc3edcd9054d1955bef3801950d17
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-08-02 14:41:05 -0700

    Add(Categories): filtered data

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,45 @@ import Home from "./pages/Home";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [mCat, setMCat] = useState([]);
+  const [fCat, setFCat] = useState([]);
+  const [jCat, setJCat] = useState([]);
+  const [eCat, setECat] = useState([]);
+
+  const fetchData = async () => {
+    const data = await axios.get("http://localhost:8000/api");
+    setItems(data.data);
+
+    let m = [];
+    let w = [];
+    let j = [];
+    let e = [];
+    for (let i = 0; i < data.data.length; i++) {
+      const element = data.data[i];
+      // console.log("OKAY", element);
+      if (element.category === "men's clothing") {
+        m.push(element);
+      } else if (element.category === "women's clothing") {
+        w.push(element);
+      } else if (element.category === "jewelery") {
+        j.push(element);
+      } else if (element.category === "electronics") {
+        e.push(element);
+      }
+    }
+    setMCat(m);
+    setFCat(w);
+    setJCat(j);
+    setECat(e);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await axios.get("http://localhost:8000/api");
-      setItems(data.data);
-      // const categories = data.data.category;
-      // console.log(categories);
-    };
     fetchData();
   }, []);
-  console.log(items);
 
   return (
     <div className="App">
-      <Home items={items} />
+      <Home items={items} mCat={mCat} fCat={fCat} jCat={jCat} eCat={eCat} />
     </div>
   );
 }
---
+++ client/src/pages/components/prodCard/Product.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: auto;
+  grid-gap: 1rem;
+  padding-top: 50px;
+`;
+
+const Card = styled.div`
+  height: 350px;
+  padding: 0.8em;
+  background: #f5f5f5;
+  position: relative;
+  overflow: visible;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+`;
+
+const IMG = styled.div`
+  background-color: #ffcaa6;
+  height: 40%;
+  width: 100%;
+  border-radius: 0.5rem;
+  transition: 0.3s ease;
+
+  &:hover {
+    transform: translateY(-25%);
+    box-shadow: rgba(226, 196, 63, 0.25) 0px 13px 47px -5px,
+      rgba(180, 71, 71, 0.3) 0px 8px 16px -8px;
+  }
+`;
+
+const CardInfo = styled.div`
+  padding-top: 10%;
+`;
+
+const TextTile = styled.p`
+  font-weight: 900;
+  font-size: 1.2em;
+  line-height: 1.5;
+`;
+
+const TextBody = styled.p`
+  font-size: 0.9em;
+  padding-bottom: 10px;
+`;
+
+const CardFooter = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 10px;
+  border-top: 1px solid #ddd;
+`;
+
+const CardButton = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid #252525;
+  display: flex;
+  padding: 0.3em;
+  cursor: pointer;
+  border-radius: 50px;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    border: 1px solid #ffcaa6;
+    background-color: #ffcaa6;
+  }
+`;
+
+const Product = () => {
+  return (
+    <CardWrapper>
+      <Card>
+        <IMG />
+        <CardInfo>
+          <TextTile>Product title </TextTile>
+          <TextBody>Product description and details</TextBody>
+        </CardInfo>
+        <CardFooter>
+          <TextTile>$499.49</TextTile>
+          <CardButton></CardButton>
+        </CardFooter>
+      </Card>
+    </CardWrapper>
+  );
+};
+
+export default Product;
