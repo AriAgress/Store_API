8ae9b0dde0756e76589dbc062cbada184bcad0bf
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-08-02 15:37:43 -0700

    Add(Product): Home page styled with cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 import Nav from "./components/nav/Nav";
 import HomeCard from "./components/prodCard/homeCard/HomeCard";
 
-const Home = () => {
+const Home = ({ items }) => {
   return (
     <div>
       <Nav />
-      <HomeCard />
+      <HomeCard items={items} />
     </div>
   );
 };
