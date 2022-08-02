2378e41d832e71d0fadab805b04c79e74b75e310
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-08-02 16:05:54 -0700

    Add(Routes): React router dom + cat pages created

---
+++ client/src/pages/EleCat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/nav/Nav";
+
+const EleCat = () => {
+  return (
+    <div>
+      <Nav />
+      <div>EleCat</div>
+    </div>
+  );
+};
+
+export default EleCat;
---
+++ client/src/pages/JewelCat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/nav/Nav";
+
+const JewelCat = () => {
+  return (
+    <div>
+      <Nav />
+      <div>JewelCat</div>
+    </div>
+  );
+};
+
+export default JewelCat;
