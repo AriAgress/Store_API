56bcf86ffefb86e27da267a3964d4f0965c6ccab
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-08-03 10:36:19 -0700

    Add(Categories): All category pages filled

--- client/src/pages/JewelCat.js
+++ client/src/pages/JewelCat.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { GiShoppingCart } from "react-icons/gi";
 import Nav from "./components/nav/Nav";
-
-const JewelCat = () => {
+import {
+  CardWrapper,
+  Card,
+  CardHeader,
+  IMG,
+  CardInfo,
+  TextTile,
+  TextBody,
+  CardFooter,
+  CardButton,
+} from "./components/prodCard/Product.js";
+const JewelCat = ({ jCat }) => {
   return (
     <div>
       <Nav />
-      <div>JewelCat</div>
+      <div>
+        <CardWrapper>
+          {jCat.map((p, i) => (
+            <Card key={i}>
+              <CardHeader>
+                <IMG src={p.image} />
+                <CardInfo>
+                  <TextTile>{p.title} </TextTile>
+                  <TextBody>{p.description}</TextBody>
+                </CardInfo>
+              </CardHeader>
+              <div>
+                <CardFooter>
+                  <TextTile>${p.price}</TextTile>
+                  <CardButton>
+                    <GiShoppingCart />
+                  </CardButton>
+                </CardFooter>
+              </div>
+            </Card>
+          ))}
+        </CardWrapper>
+      </div>
     </div>
   );
 };
